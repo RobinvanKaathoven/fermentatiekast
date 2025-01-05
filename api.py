eb2e942608192d070ca194115aadd536ca1376cf
Robin <rjmvankaathoven@gmail.com>
2025-01-05 21:54:04 +0100

    Adding a database and first model for Fermentation

--- api.py
+++ api.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fermentation.db' # Using SQLite for simplicity
+db = SQLAlchemy(app)
+
+class Fermentation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    startDate = db.Column(db.DateTime, nullable=False)
+    endDate = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<Fermentation {self.name} from {self.startDate}>'
 
 @app.route('/')
 def home():
