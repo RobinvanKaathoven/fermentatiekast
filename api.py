55dc02ec5aa2e06e299e9cf4520aec1e877fa466
Robin <rjmvankaathoven@gmail.com>
2025-01-06 21:49:22 +0100

    added more rest stuffs

--- api.py
+++ api.py
@@ -14,8 +14,8 @@ class Fermentation(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
 
-    startDate = db.Column(db.DateTime, nullable=False)
-    endDate = db.Column(db.DateTime, nullable=False)
+    startDate = db.Column(db.DateTime, nullable=True)
+    endDate = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
         return f'<Fermentation {self.name} from {self.startDate}>'
@@ -24,9 +24,17 @@ fermentation_args = reqparse.RequestParser()
 fermentation_args.add_argument('name', type=str, help='Name of the fermentation', required=True)
 fermentation_args.add_argument('temperature', type=float, help='Requested temperature of the fermentation', required=True)
 fermentation_args.add_argument('duration', type=int, help='Duration of the fermentation in days', required=True)
+fermentationFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'temperature': fields.Float,
+    'duration': fields.Integer,
+    'startDate': fields.DateTime,
+    'endDate': fields.DateTime
+}
 
-class FermentationResource(Resource):
-    @marshal_with(fermentation_args)
+class FermentationsResource(Resource):
+    @marshal_with(fermentationFields)
     def get(self):
         print('get all fermentations')
         fermentations = Fermentation.query.all()
@@ -34,23 +42,47 @@ class FermentationResource(Resource):
             abort(404, message="No fermentations found")
         return fermentations
     
-    @marshal_with(fermentation_args)
+    @marshal_with(fermentationFields)
+    def post(self):
+        args = fermentation_args.parse_args()
+        fermentation = Fermentation(name=args['name'], temperature=args['temperature'], duration=args['duration'])
+        db.session.add(fermentation)
+        db.session.commit()
+        return fermentation, 201
+    
+    
+class FermentationResource(Resource):
+
+    
+    @marshal_with(fermentationFields)
     def get(self, fermentation_id):
-        print(fermentation_id)
         fermentation = Fermentation.query.get(fermentation_id)
         if not fermentation:
             abort(404, message="Fermentation not found")
         return fermentation
 
-    @marshal_with(fermentation_args)
-    def post(self, fermentation_id):
+    @marshal_with(fermentationFields)
+    def patch(self, fermentation_id):
+        print(fermentation_id)
         args = fermentation_args.parse_args()
-        fermentation = Fermentation(id=fermentation_id, **args)
-        db.session.add(fermentation)
+        fermentation = Fermentation.query.get(fermentation_id)
+        if not fermentation:
+            abort(404, message="Fermentation not found")
+        if 'name' in args:
+            fermentation.name = args['name']
+        if 'temperature' in args:
+            fermentation.temperature=args['temperature']
+        if 'duration' in args:
+            fermentation.duration=args['duration']
+        if 'startDate' in args:
+            fermentation.startDate=args['startDate']
+        if 'endDate' in args:
+            fermentation.endDate=args['endDate']      
         db.session.commit()
         return fermentation, 201
 
 api.add_resource(FermentationResource, '/api/fermentation/<int:fermentation_id>')
+api.add_resource(FermentationsResource, '/api/fermentation/')
 
 @app.route('/')
 def home():
