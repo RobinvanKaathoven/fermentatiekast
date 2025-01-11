a58cca165da67310820aec0b99543d8ec3e921b2
Robin <rjmvankaathoven@gmail.com>
2025-01-11 20:39:20 +0000

    moved Metric out to support directory

--- application/sensors/temperature.py
+++ application/sensors/temperature.py
@@ -1,12 +1,4 @@
-class Metric:
-    def __init__(self, name, value, labels={}):
-        self.name = name
-        self.labels = labels
-        self.value = value
-    
-    def to_prometheus(self):
-        labels = ', '.join([f"{key}=\"{val}\"" for key, val in self.labels.items()])
-        return f"{self.name}{{{labels}}} {self.value}"
+from helpers.metric import Metric
     
 class TemperatureSensor:
     def __init__(self, data_pin):
