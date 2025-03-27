a113552d109c11fe01ec8a04fe180749f0817ea0
Robin <rjmvankaathoven@gmail.com>
2025-03-27 08:18:40 +0100

    Removed files, among them the now unused Rule class

--- application/ruleEngine/Rule.py
+++
@@ -1,7 +0,0 @@
-class Rule():
-    def __init__(self, name, validationFunction, controlFunction):
-        self.name = name
-        self.validationFunction = validationFunction
-        self.controlFunction = controlFunction
-    def evaluate(self, temperature, humidity):
-        self.controlFunction(self.validationFunction(temperature, humidity))
