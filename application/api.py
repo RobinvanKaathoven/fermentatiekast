4db15fa9dbebc49a074e2812e75c401cd857a920
Robin <rjmvankaathoven@gmail.com>
2025-03-27 08:17:45 +0100

    refactored Fermentation to be a separate class and added a resource to mock temperature changes

--- application/api.py
+++ application/api.py
@@ -2,96 +2,49 @@ import os, sys
 import time
 from flask import Flask
 from flask import render_template 
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_swagger_ui import get_swaggerui_blueprint
+from util import db
 
 from ruleEngine.RuleEngine import RuleEngine
 from sensors.temperature import TemperatureSensor
 from threading import Thread
 
-from ruleEngine.Rule import Rule
+from ruleEngine.Controller import Controller
+from fermentation.FermentationResource import *
 
-
+DEBUG_MODE = True
 print("Things have been imported")
 
 app = Flask(__name__, template_folder="templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fermentation.db' # Using SQLite for simplicity
-
-db = SQLAlchemy(app)
+db.init_app(app)
 api = Api(app)
-class Fermentation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    temperature = db.Column(db.Float, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
-
-    startDate = db.Column(db.DateTime, nullable=True)
-    endDate = db.Column(db.DateTime, nullable=True)
-
-    def __repr__(self):
-        return f'<Fermentation {self.name} from {self.startDate}>'
-fermentation_args = reqparse.RequestParser()
-fermentation_args.add_argument('name', type=str, help='Name of the fermentation', required=True)
-fermentation_args.add_argument('temperature', type=float, help='Requested temperature of the fermentation', required=True)
-fermentation_args.add_argument('duration', type=int, help='Duration of the fermentation in days', required=True)
-fermentationFields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'temperature': fields.Float,
-    'humidity': fields.Integer,
-    'duration': fields.Integer,
-    'startDate': fields.DateTime,
-    'endDate': fields.DateTime
-}
-
-class FermentationsResource(Resource):
-    @marshal_with(fermentationFields)
-    def get(self):
-        print('get all fermentations')
-        fermentations = Fermentation.query.all()
-        if not fermentations:
-            abort(404, message="No fermentations found")
-        return fermentations
-    
-    @marshal_with(fermentationFields)
-    def post(self):
-        args = fermentation_args.parse_args()
-        fermentation = Fermentation(name=args['name'], temperature=args['temperature'], duration=args['duration'])
-        db.session.add(fermentation)
-        db.session.commit()
-        return fermentation, 201
-    
-class FermentationResource(Resource):
-    @marshal_with(fermentationFields)
-    def get(self, fermentation_id):
-        fermentation = Fermentation.query.get(fermentation_id)
-        if not fermentation:
-            abort(404, message="Fermentation not found")
-        return fermentation
-
-    @marshal_with(fermentationFields)
-    def patch(self, fermentation_id):
-        print(fermentation_id)
-        args = fermentation_args.parse_args()
-        fermentation = Fermentation.query.get(fermentation_id)
-        if not fermentation:
-            abort(404, message="Fermentation not found")
-        if 'name' in args:
-            fermentation.name = args['name']
-        if 'temperature' in args:
-            fermentation.temperature=args['temperature']
-        if 'duration' in args:
-            fermentation.duration=args['duration']
-        if 'startDate' in args:
-            fermentation.startDate=args['startDate']
-        if 'endDate' in args:
-            fermentation.endDate=args['endDate']      
-        db.session.commit()
-        return fermentation, 201
 
 api.add_resource(FermentationResource, '/api/fermentation/<int:fermentation_id>')
 api.add_resource(FermentationsResource, '/api/fermentation/')
+
+mock_args = reqparse.RequestParser()
+mock_args.add_argument('temperature', type=float, help='Temperature of the mock sensor', required=True)
+mock_args.add_argument('humidity', type=int, help='Humidity of the mock sensor', required=True)
+mockFields = {
+    'temperature': fields.Float,
+    'humidity': fields.Integer
+}
+
+class MockResource(Resource):
+    @marshal_with(mockFields)
+    def get(self):
+        return {'temperature': temperatureSensor.temperature, 'humidity': temperatureSensor.humidity}
+
+    @marshal_with(mockFields)
+    def post(self):
+        args = mock_args.parse_args()
+        temperatureSensor.set(args['temperature'], args['humidity'])
+        return {'temperature': temperatureSensor.temperature, 'humidity': temperatureSensor.humidity}
+api.add_resource(MockResource, '/api/mock/')
+
 
 @app.route('/')
 def home():
---
+++ application/fermentation/FermentationResource.py
@@ -0,0 +1,74 @@
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
+from util.db import db
+
+
+class Fermentation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    startDate = db.Column(db.DateTime, nullable=True)
+    endDate = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f'<Fermentation {self.name} from {self.startDate}>'
+    
+fermentation_args = reqparse.RequestParser()
+fermentation_args.add_argument('name', type=str, help='Name of the fermentation', required=True)
+fermentation_args.add_argument('temperature', type=float, help='Requested temperature of the fermentation', required=True)
+fermentation_args.add_argument('duration', type=int, help='Duration of the fermentation in days', required=True)
+fermentationFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'temperature': fields.Float,
+    'humidity': fields.Integer,
+    'duration': fields.Integer,
+    'startDate': fields.DateTime,
+    'endDate': fields.DateTime
+}
+
+class FermentationsResource(Resource):
+    @marshal_with(fermentationFields)
+    def get(self):
+        print('get all fermentations')
+        fermentations = Fermentation.query.all()
+        if not fermentations:
+            abort(404, message="No fermentations found")
+        return fermentations
+    
+    @marshal_with(fermentationFields)
+    def post(self):
+        args = fermentation_args.parse_args()
+        fermentation = Fermentation(name=args['name'], temperature=args['temperature'], duration=args['duration'])
+        db.session.add(fermentation)
+        db.session.commit()
+        return fermentation, 201
+
+class FermentationResource(Resource):
+    @marshal_with(fermentationFields)
+    def get(self, fermentation_id):
+        fermentation = Fermentation.query.get(fermentation_id)
+        if not fermentation:
+            abort(404, message="Fermentation not found")
+        return fermentation
+
+    @marshal_with(fermentationFields)
+    def patch(self, fermentation_id):
+        print(fermentation_id)
+        args = fermentation_args.parse_args()
+        fermentation = Fermentation.query.get(fermentation_id)
+        if not fermentation:
+            abort(404, message="Fermentation not found")
+        if 'name' in args:
+            fermentation.name = args['name']
+        if 'temperature' in args:
+            fermentation.temperature=args['temperature']
+        if 'duration' in args:
+            fermentation.duration=args['duration']
+        if 'startDate' in args:
+            fermentation.startDate=args['startDate']
+        if 'endDate' in args:
+            fermentation.endDate=args['endDate']      
+        db.session.commit()
+        return fermentation, 201
